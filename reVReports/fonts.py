bb99135ff5c1fe918989300ba7ac6e57dcc03c13
ppinchuk <pinchukpaul@gmail.com>
2025-11-13 16:43:03 -0700

    Formatting

--- reVReports/fonts.py
+++ reVReports/fonts.py
@@ -1,15 +1,16 @@
-# -*- coding: utf-8 -*-
-"""fonts module"""
+"""Fonts module"""
+
 from matplotlib import font_manager
 
 from reVReports import PACKAGE_DIR
 
 SANS_SERIF = font_manager.FontEntry(
-    fname=PACKAGE_DIR.joinpath("assets", "LocalDejaVuSans.ttf"), name="LocalDejaVuSans"
+    fname=PACKAGE_DIR / "assets" / "LocalDejaVuSans.ttf",
+    name="LocalDejaVuSans",
 )
 
 SANS_SERIF_BOLD = font_manager.FontEntry(
-    fname=PACKAGE_DIR.joinpath("assets", "LocalDejaVuSans-Bold.ttf"),
+    fname=PACKAGE_DIR / "assets" / "LocalDejaVuSans-Bold.ttf",
     weight="bold",
     name="LocalDejaVuSans",
 )

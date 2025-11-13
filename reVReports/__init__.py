69a68a246d22e2f535466b402887c3ce20eb244a
ppinchuk <pinchukpaul@gmail.com>
2025-11-13 16:42:54 -0700

    Minor change

--- reVReports/__init__.py
+++ reVReports/__init__.py
@@ -5,4 +5,4 @@ from pathlib import Path
 from reVReports.version import __version__
 
 PACKAGE_DIR = Path(__file__).parent
-DATA_DIR = PACKAGE_DIR.joinpath("datasets")
+DATA_DIR = PACKAGE_DIR / "datasets"
